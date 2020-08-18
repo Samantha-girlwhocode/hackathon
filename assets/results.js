47171ef7db38b26ee830f3845a2b22d1f4812c64
kwalker18 <69206959+kwalker18@users.noreply.github.com>
2020-08-18 13:04:13 -0400

    Add files via upload

---
+++ assets/results.js
@@ -0,0 +1,11 @@
+function showResults(){
+
+    if (englishCounter > 5) {
+        <p> You get English!</p>
+    } else if (spanishCounter > 5) {
+        <p> You get Spanish! </p>
+    } else if (frenchCounter > 5) {
+        <p> You get French!</p>
+    }
+    //
+}
