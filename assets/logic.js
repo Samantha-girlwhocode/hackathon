6f93cab5bc21d44c9e9c450973fa57d07afc6c04
Samantha-girlwhocode <samantha.kaufman48@girlswhocode.com>
2020-08-20 11:18:51 -0500

    Styled index.html, button funtionalty

---
+++ assets/logic.js
@@ -0,0 +1,56 @@
+var currentQuestionIndex = 0;
+var questionsE1 = document.getElementById("questions");
+var choicesE1 = document.getElementById("choices");
+var startBtn = document.getElementById("start");
+function startQuiz() {
+    // hide start screen
+    var startScreenEl = document.getElementById("start-screen");
+    questionsE1.removeAttribute("class");
+    getQuestion();
+  }
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    choicesE1.innerHTML = "";
+    // loop over choices
+  currentQuestion.choices.forEach(function(choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = i + 1 + ". " + choice;
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+    // display on the page
+    choicesE1.appendChild(choiceNode);
+});
+}
+function questionClick() {
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+  function quizEnd() {
+    var resultsPage = document.getElementById("results");
+    resultsPage.removeAttribute("class");
+  }
+startBtn.onclick = startQuiz;
+console.log(questions)
+
+
+
+
+
+
+
+
+
+
+
--- assets/questions.js
+++ assets/questions.js
@@ -1,43 +1,37 @@
-var englishCounter = 0
-var spanishCounter = 0
-var portugeseCounter = 0
-var italianCounter = 0
-
-
+var userPoints = 0
 var questions = [
     {
         title: "What's your native language?",
-        choices: ["English", "Spanish", "Portugese", "Italian", "R"]
+        choices: ["English", "Spanish", "French", "Italian", "Mandarin"]
     },
     {
-        title: "Which language(s) do you speak?",
-        choices: ["English", "Spanish", "French", "Portuguese", "Arabic", "Russian", "Japanese", "Hindi", "Tamil", "Korean", "Indonesian", "Korean", "Turkish"],
-        function checkBox() {
-        var checkboxes = document.getElementById("check")
-                 if(checkboxes.checked) {
-        //record this for the later analysis
-    } else {
-        //don't
+        title: "Do you want to learn an easy or more challenging language?",
+        choices: ["Easy", "Medium", "Hard"]
     },
-            },
-
+    {
+        title: "How much time do you have to learn it?",
+        choices: ["Months", "Years", "My Whole Life"]
     },
-{
-    title: "Do you want to learn an easy or more challenging language?"
-    choices: ["Easy", "Medium", "Hard"]
-
-    //TODO make a slider bar with circles to determine difficulty
-},
-{
-    title: "How much time do you have to learn it?"
-    choices: ["A lot (1+ hour/day)", "More than most (40+ minutes/day)", "An average amount (25+ minutes/day", "A little (<25 minutes/day"]
-},
-title: "Do you want a language with a latin alphabet or a different writing system ex: Chinese characters, Russian Cryllic, etc.."
-choices: ["Latin alphabet", "I don't care which writing system", "Hanzi", "Any non-latin script"]
-    // TODO do the points thing Julissa was talking about.  
-    //The more points you have, the harder the language is
-    //so if the person picks latin alphabet give them the least amount of points 
+    {
+        title: "Which of these do you prefer in your language?",
+        choices: ["Alphabet", "Syllabic Alphabets", "Semanto-phonetic writing systems"]
+    },
+    {
+        title: "Do you prefer a language that is more universal or more uncommon?",
+        choices: [ "More uncommon", "A good in-between", "More universal"]
+    }
+];
+console.log(questions);
+if (questions[0].choices === [0]) {
+    userPoints ++
+} else if (questions[0].choices === [1]) {
+    userPoints + 2 
+} else if (questions[0].choices === [2]) {
+    userPoints + 3 
+} else if (questions[0].choices === [3]) {
+    userPoints + 4 
+} else if (questions[0].choices === [4]) {
+    userPoints + 5 
 }
-var 
-        
-]
+console.log(userPoints)
+
